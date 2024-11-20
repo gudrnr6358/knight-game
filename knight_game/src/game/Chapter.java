c07c2dac054943737cc652714ee3c0e4c9458874
Hinggik <gudrnr6358@gmail.com>
2024-11-20 23:27:18 +0900

    주승현 Chapter Stage

--- knight_game/src/game/Chapter.java
+++ knight_game/src/game/Chapter.java
@@ -12,12 +12,14 @@ public class Chapter extends JPanel {
 	private CardLayout cardLayout;
 	private JPanel cardPanel;
 	private MyListener listener;
+	private boolean[] stageClear = new boolean[3];
 
-	public Chapter() {
+	public Chapter(CardLayout cardLayout, JPanel cardPanel) {
 		this.cardLayout = cardLayout;
 		this.cardPanel = cardPanel;
 		setLayout(null);
 		listener = new MyListener();
+		stageClear = new boolean[3];
 
 		// Chapter 1 버튼
 		buttons[0].setBounds(200, 200, 100, 100);
@@ -28,11 +30,24 @@ public class Chapter extends JPanel {
 		buttons[1].setBounds(400, 200, 100, 100);
 		add(buttons[1]);
 		buttons[1].addActionListener(listener);
+		buttons[1].setEnabled(false);
 
 		// 돌아가기 버튼
 		buttons[2].setBounds(600, 200, 100, 100);
 		add(buttons[2]);
 		buttons[2].addActionListener(listener);
+	}
+
+	public void updateStageClear(int stageIndex, boolean isClear) {
+		stageClear[stageIndex] = isClear;
+		updateChapter();
+	}
+
+	// chapter2 버튼 활성화
+	public void updateChapter() {
+		if (stageClear[0] && stageClear[1] && stageClear[2]) {
+			buttons[1].setEnabled(true);
+		}
 	}
 
 	class MyListener implements ActionListener {
