a9105a9aba576932f16ecce6ec52038db2c91601
david <165134399+david7643@users.noreply.github.com>
2024-11-21 00:24:29 +0900

    Update Character.java

--- knight_game/src/game/Character.java
+++ knight_game/src/game/Character.java
@@ -1,57 +1,35 @@
 package game;
-
+import javax.swing.*;
 import javax.swing.ImageIcon;
-
-public class Character implements Combatant, ImageUnit {
+import java.awt.*;
+public class Character extends Combatant{
 	public static boolean hadCharacter = false;
-	public String name;
-	public Integer level;
-	public Integer exp;
-	public Integer hp;
-	public Integer nowHp;
-	public Integer power;
-	public Boolean useSkill;
 	private Integer[] EXP = { 20, 30, 35, 40, 45, 70, 80, 90, 110 };
 	private Integer[] LEVEL_UP_PLUS_HP = { 10, 10, 15, 15, 15, 20, 20, 25, 30 };
 	private Integer[] LEVEL_UP_PLUS_POWER = { 10, 10, 15, 15, 15, 20, 20, 25, 30 };
-
-	// 이름만 받아서 객체 생성, 이름 설정 Writer Reader 이용해도 괜찮을 듯
+	public Integer exp;
+	public Integer level;
+	public Integer level_count;
+	public Integer hp;
 	public Character() {
 		this.level = 1;
+		this.level_count=0;
 		this.exp = 0;
-		this.hp = 100;
-		this.nowHp = 100;
-		this.power = 10;
 	}
-
-	@Override
-	public int attack() {
-		// TODO Auto-generated method stub
-		return 10;
-	}
-
-	@Override
-	public int skill() {
-		// TODO Auto-generated method stub
-		return 10;
-	}
-
-	public int charSkill() {
-		// TODO Auto-generated method stub
-		return 10;
-	}
-
-	@Override
-	public ImageIcon getImage() {
-		return new ImageIcon("images/knight.png");
-	}
-
-	@Override
-	public boolean isAlive() {
-		if (nowHp > 0) {
-			return true;
+	public static void levelup() {
+		if(level_count == EXP.length) {
+			level++;
+			level_count++;
+			hp += LEVEL_UP_PLUS_HP[level_count];
+			power += LEVEL_UP_PLUS_POWER[level_count];
+			nowHp = hp;
+			new JLabel("현재 레벨:" + level + " (" + exp + "/" + EXP[level_count] + ")");
 		}
-		return false;
+		else
+			new JLabel("현재 최고레벨을 달성하여 레벨업이 불가합니다.");
 	}
-
+	public void recoveryHp() {
+		nowHp=hp;
+		new JLabel("HP를 모두 회복했습니다!");
+	}
 }
