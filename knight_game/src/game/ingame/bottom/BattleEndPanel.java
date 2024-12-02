a6b096a5ba68fb3c218d529222b3bb3a924304f6
Hinggik <gudrnr6358@gmail.com>
2024-12-02 20:11:55 +0900

    2024-12-02-20:11 BattleEndPanel 수정

--- knight_game/src/game/ingame/bottom/BattleEndPanel.java
+++ knight_game/src/game/ingame/bottom/BattleEndPanel.java
@@ -2,11 +2,8 @@ package game.ingame.bottom;
 
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.Graphics;
 
 import javax.swing.JLabel;
-
-import game.InGamePanel;
 
 // 전투 종료 후를 관리하는 메서드
 public class BattleEndPanel extends BottomBox {
@@ -18,10 +15,7 @@ public class BattleEndPanel extends BottomBox {
 
 	private class NextPageInfoLabel extends JLabel {
 
-		private static String str;
-
 		private NextPageInfoLabel() {
-			super(str);
 			setForeground(new Color(128, 128, 128));
 			setFont(new Font("SansSerif", Font.BOLD, 22));
 			setBounds(1040, 70, 300, 470);
@@ -33,15 +27,15 @@ public class BattleEndPanel extends BottomBox {
 
 			// 캐릭터 사망
 			if (!INGAME.character.isAlive()) {
-				str = "클릭 시 로비로 돌아갑니다";
+				setText("클릭 시 로비로 돌아갑니다");
 				return this;
 			}
 			// 마지막 몬스터 처치
 			if (!INGAME.monsters[INGAME.monsters.length - 1].isAlive()) {
-				str = "클릭 시 로비로 돌아갑니다";
+				setText("클릭 시 로비로 돌아갑니다");
 				return this;
 			}
-			str = "진행하려면 클릭하세요";
+			setText("진행하려면 클릭하세요");
 			return this;
 		}
 	}
