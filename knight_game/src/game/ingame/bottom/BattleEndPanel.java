1059137e82f437e6a48a97fdaa9e7c02262828be
Hinggik <gudrnr6358@gmail.com>
2024-12-03 12:01:43 +0900

    2024-12-03-12:01 BattleEndPanel.NextPageInfoLabel 텍스트 위치 세팅 수정

--- knight_game/src/game/ingame/bottom/BattleEndPanel.java
+++ knight_game/src/game/ingame/bottom/BattleEndPanel.java
@@ -15,10 +15,17 @@ public class BattleEndPanel extends BottomBox {
 
 	private class NextPageInfoLabel extends JLabel {
 
+		// 기본 값 세팅
+		private Integer x = 1040;
+		private Integer y = 70;
+		private Integer width = 300;
+		private Integer height = 470;
+
 		private NextPageInfoLabel() {
 			setForeground(new Color(128, 128, 128));
 			setFont(new Font("SansSerif", Font.BOLD, 22));
-			setBounds(1040, 70, 300, 470);
+			setBounds(x, y, width, height);
+			repaint();
 		}
 
 		// 우측 하단 출력할 문자열 정하는 메서드
@@ -36,6 +43,8 @@ public class BattleEndPanel extends BottomBox {
 				return this;
 			}
 			setText("진행하려면 클릭하세요");
+			x = 1040 + 35;
+			setBounds(x, y, width, height);
 			return this;
 		}
 	}
